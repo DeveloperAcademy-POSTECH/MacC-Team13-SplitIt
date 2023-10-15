31d44940c2037e594d720769e920bd95e15512fe
hsw1920 <hsw1920@gmail.com>
2023-10-15 20:24:55 +0900

    [Feat/#1] HomeView 세팅

--- SplitIt/SceneDelegate.swift
+++ SplitIt/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootVC = ViewController()
+        let rootVC = HomeVC()
         let navigationController = UINavigationController(rootViewController: rootVC)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
