7baf2ca418a0a67f8ab8144e13e518604c2c1d74
hsw1920 <hsw1920@gmail.com>
2023-10-18 18:09:52 +0900

    [Feat/#20] ExclMember 뷰의 레이아웃 및 Dummy 데이터로 ViewModel의 비즈니스 로직 구현

--- SplitIt/Presenter/SplitCreateFlow/ExclItemCycle/ExclItemMember/ExclMemberSection.swift
+++ SplitIt/Presenter/SplitCreateFlow/ExclItemCycle/ExclItemMember/ExclMemberSection.swift
@@ -5,4 +5,53 @@
 //  Created by 홍승완 on 2023/10/18.
 //
 
-import Foundation
+import RxDataSources
+
+struct TargetSectionModel {
+    var type: TargetSectionType
+}
+
+enum TargetSectionType {
+    case data(TargetSection)
+    case button(TargetSection)
+}
+
+struct TargetSection {
+    var title: String
+    var price: String
+    var items: [Target]
+    static let button: TargetSection = TargetSection(title: "술", price: "9,000", items: [Target(name: "Button", isTarget: false)])
+}
+
+struct Target: IdentifiableType, Equatable {
+    let name: String
+    let isTarget: Bool
+    
+    // IdentifiableType
+    var identity: String {
+        return name
+    }
+    
+    // Equatable
+    static func == (lhs: Target, rhs: Target) -> Bool {
+        return lhs.identity == rhs.identity
+    }
+}
+
+extension TargetSectionModel: SectionModelType {
+    typealias Item = Target // 섹션의 아이템 타입
+    var items: [Item] {
+        switch type {
+        case .data(let targets):
+            return targets.items
+        case .button(let target):
+            return target.items
+        }
+    }
+    
+    init(original: TargetSectionModel, items: [Item]) {
+        self = original
+    }
+}
+
+
--- SplitIt/Presenter/SplitCreateFlow/ExclItemCycle/ExclItemMember/ViewModel/ExclMemberVM.swift
+++ SplitIt/Presenter/SplitCreateFlow/ExclItemCycle/ExclItemMember/ViewModel/ExclMemberVM.swift
@@ -5,4 +5,59 @@
 //  Created by 홍승완 on 2023/10/18.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
+import UIKit
+
+class ExclMemberVM {
+    
+    var disposeBag = DisposeBag()
+    
+    let sections = BehaviorRelay<[TargetSectionModel]>(value: [])
+
+    struct Input {
+        let viewDidLoad: Driver<Void> // viewDidLoad
+        let nextButtonTapSend: Driver<Void> // 다음 버튼
+    }
+    
+    struct Output {g
+        let presentResultView: Driver<Void>
+    }
+    
+    func transform(input: Input) -> Output {
+        let presentResultView = input.nextButtonTapSend.asDriver()
+        
+        input.viewDidLoad
+            .drive(onNext: { [weak self] in
+                guard let self = self else { return }
+                
+                // section 입력
+                let items1 = [Target(name: "나", isTarget: false),Target(name: "하태민", isTarget: false), Target(name: "제롬", isTarget: false), Target(name: "완", isTarget: false), Target(name: "조채원", isTarget: false)]
+                let dataSection = TargetSection(title: "술", price: "9,000", items: items1)
+                let dataSectionModel = TargetSectionModel(type: .data(dataSection))
+                
+                let items2 = [Target(name: "가나다라마바", isTarget: false),Target(name: "가나다", isTarget: false), Target(name: "가나", isTarget: false), Target(name: "가나다라", isTarget: false), Target(name: "키득키득입니다만", isTarget: false)]
+                let dataSection2 = TargetSection(title: "돼지고기", price: "11,000", items: items2)
+                let dataSectionModel2 = TargetSectionModel(type: .data(dataSection2))
+                
+                let items3 = [Target(name: "안녕", isTarget: false),Target(name: "하세요", isTarget: false), Target(name: "반갑습니다", isTarget: false), Target(name: "어쩌라구요", isTarget: false), Target(name: "몰라요", isTarget: false), Target(name: "으아악", isTarget: false),Target(name: "안녕", isTarget: false),Target(name: "하세요", isTarget: false), Target(name: "반갑습니다", isTarget: false), Target(name: "어쩌라구요", isTarget: false), Target(name: "몰라요", isTarget: false), Target(name: "으아악", isTarget: false)]
+                let dataSection3 = TargetSection(title: "백화점", price: "5,500,000", items: items3)
+                let dataSectionModel3 = TargetSectionModel(type: .data(dataSection3))
+                
+                let buttonSectionModel = TargetSectionModel(type: .button(TargetSection.button))
+                
+                sections.accept([dataSectionModel,dataSectionModel2,dataSectionModel3, buttonSectionModel])
+            })
+            .disposed(by: disposeBag)
+
+        presentResultView
+            .drive(onNext: {
+                // MARK: ResultView 로 전환될 때의 비즈니스 로직
+
+            })
+            .disposed(by: disposeBag)
+
+        return Output(presentResultView: presentResultView)
+    }
+}
+
