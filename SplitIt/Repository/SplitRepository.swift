cbae98e59f0d398f0069e1d449f808be99fcef79
Jhwan <ehgp77@naver.com>
2023-10-18 23:07:29 +0900

    [Feat/#25] 더미데이터 생성 함수추가-2???

--- SplitIt/Repository/SplitRepository.swift
+++ SplitIt/Repository/SplitRepository.swift
@@ -494,3 +494,28 @@ extension SplitRepository {
         realmManager.updateData(exclMemberArr: exclMemberArr)
     }
 }
+
+//extension SplitRepository {
+//    func createExclItemPrice(price: Int) {
+//        
+//    }
+//}
+
+
+func createDummy() {
+    let repo = SplitRepository.share
+    
+    repo.createDatasForCreateFlow()
+    repo.inputCSInfoWithTitle(title: "Test")
+    repo.inputCSInfoWithTotalAmount(totalAmount: 1000)
+    repo.createCSMember(name: "제롬")
+    repo.createCSMember(name: "완")
+    repo.createCSMember(name: "모아나")
+    repo.createCSMember(name: "제리")
+    repo.createCSMember(name: "토마토")
+    repo.createCSMember(name: "코리")
+    repo.createExclItemWithName(name: "맥주")
+    repo.inputExclItemPrice(price: 100)
+    repo.updateDataToDB()
+}
+
