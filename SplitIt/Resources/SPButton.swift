7fd325b7084d410ca148d7f8b53a2e0f8e59078c
pscu91 <a95690181@gmail.com>
2023-10-19 23:15:09 +0900

    [Feat/#24] Half Button 디자인 case 정의

--- SplitIt/Resources/SPButton.swift
+++ SplitIt/Resources/SPButton.swift
@@ -13,7 +13,7 @@ import SnapKit
 extension SPButton {
     enum Style {
 
-        // MARK: Active Button Styles
+        // MARK: Active Normal Button Styles
         case primaryCalmshell
         case primaryWatermelon
         case primaryCherry
@@ -23,7 +23,7 @@ extension SPButton {
         case secondary
         case warningRed
         
-        // MARK: Pressed Button Styles
+        // MARK: Pressed Normal Button Styles
         case primaryCalmshellPressed
         case primaryWatermelonPressed
         case primaryCherryPressed
@@ -33,17 +33,36 @@ extension SPButton {
         case secondaryPressed
         case warningRedPressed
         
-        // MARK: Square Button
+        // MARK: Active Square Button Styles
         case squarePrimaryCalmshell
         case squareWarningRed
+        
+        // MARK: Active Half Button Styles
+        case halfSmartSplit
+        case halfEqualSplit
+        case halfShare
+        case halfExit
+        
+        // MARK: Pressed Half Button Styles
+        case halfSmartSplitPressed
+        case halfEqualSplitPressed
+        case halfSharePressed
+        case halfExitPressed
         
         // MARK: Deactive Button Style
         case deactivate
         case squareDeactivate
+        case halfSmartSplitDeactivate
+        case halfEqualSplitDeactivate
+        case halfShareDeactivate
+        case halfExitDeactivate
     }
 }
 
 final class SPButton: UIButton {
+    let currencyIcon = UIImageView()
+    let currencyLabel = UILabel()
+
     // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -56,6 +75,8 @@ final class SPButton: UIButton {
     // MARK: - Style Configuration
     func applyStyle(_ style: Style) {
         configureCommonProperties()
+        setCurrencyIcon(style: style)
+        setCurrencyLabel(style: style)
         
         switch style {
         // 활성 버튼, Unpressed 상태
@@ -156,7 +177,7 @@ final class SPButton: UIButton {
             self.configureActiveProperties()
             self.configurePressedProperties()
             
-        // 사각 버튼
+        // 활성 사각 버튼
         case .squarePrimaryCalmshell:
             self.backgroundColor = .SurfaceBrandCalmshell
             self.configureCommonPropertiesForSquare()
@@ -169,14 +190,90 @@ final class SPButton: UIButton {
             self.configureActiveProperties()
             self.configureUnpressedProperties()
             
+        // 활성 하프 버튼
+        case .halfSmartSplit:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configureUnpressedProperties()
+            
+        case .halfEqualSplit:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configureUnpressedProperties()
+        
+        case .halfShare:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configureUnpressedProperties()
+            
+        case .halfExit:
+            self.backgroundColor = .SurfaceSecondary
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configureUnpressedProperties()
+            
+        // 비활성, 하프 버튼 Pressed 상태
+        case .halfSmartSplitPressed:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configurePressedProperties()
+            
+        case .halfEqualSplitPressed:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configurePressedProperties()
+        
+        case .halfSharePressed:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configurePressedProperties()
+            
+        case .halfExitPressed:
+            self.backgroundColor = .SurfaceSecondary
+            self.configureCommonPropertiesForHalf()
+            self.configureActiveProperties()
+            self.configurePressedProperties()
+            
         // 비활성 버튼
         case .deactivate:
             self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForNormal()
             self.configureDeactiveProperties()
             self.configurePressedProperties()
             
-        case . squareDeactivate:
+        case .squareDeactivate:
             self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForSquare()
+            self.configureDeactiveProperties()
+            self.configurePressedProperties()
+            
+        case .halfSmartSplitDeactivate:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureDeactiveProperties()
+            self.configurePressedProperties()
+            
+        case .halfEqualSplitDeactivate:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureDeactiveProperties()
+            self.configurePressedProperties()
+        
+        case .halfShareDeactivate:
+            self.backgroundColor = .SurfaceBrandCalmshell
+            self.configureCommonPropertiesForHalf()
+            self.configureDeactiveProperties()
+            self.configurePressedProperties()
+            
+        case .halfExitDeactivate:
+            self.backgroundColor = .SurfaceSecondary
+            self.configureCommonPropertiesForHalf()
             self.configureDeactiveProperties()
             self.configurePressedProperties()
         }
@@ -198,6 +295,11 @@ final class SPButton: UIButton {
     // 사각 버튼 프로퍼티
     private func configureCommonPropertiesForSquare() {
         self.layer.cornerRadius = 8
+    }
+    
+    // 하프 버튼 프로퍼티
+    private func configureCommonPropertiesForHalf() {
+        self.layer.cornerRadius = 42
     }
     
     // 활성 상태 프로퍼티
@@ -224,5 +326,202 @@ final class SPButton: UIButton {
     private func configurePressedProperties() {
         self.isEnabled = false
         self.layer.shadowOffset = CGSize(width: 0, height: 3)
+    }
+    
+    private func setCurrencyIcon(style: SPButton.Style) {
+        self.addSubview(currencyIcon)
+        
+        currencyIcon.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(80)
+            $0.top.equalToSuperview().inset(14)
+        }
+        
+        switch style {
+        case .primaryCalmshell:
+            break
+        case .primaryWatermelon:
+            break
+        case .primaryCherry:
+            break
+        case .primaryPear:
+            break
+        case .primaryMushroom:
+            break
+        case .primaryRadish:
+            break
+        case .secondary:
+            break
+        case .warningRed:
+            break
+        case .primaryCalmshellPressed:
+            break
+        case .primaryWatermelonPressed:
+            break
+        case .primaryCherryPressed:
+            break
+        case .primaryPearPressed:
+            break
+        case .primaryMushroomPressed:
+            break
+        case .primaryRadishPressed:
+            break
+        case .secondaryPressed:
+            break
+        case .warningRedPressed:
+            break
+        case .squarePrimaryCalmshell:
+            break
+        case .squareWarningRed:
+            break
+        case .deactivate:
+            break
+        case .squareDeactivate:
+            break
+        case .halfSmartSplit:
+            currencyIcon.image = UIImage(named:"SplitIconDefault")
+        case .halfEqualSplit:
+            currencyIcon.image = UIImage(named:"EqualIconDefault")
+        case .halfShare:
+            currencyIcon.image = UIImage(named:"ShareIconDefault")
+        case .halfExit:
+            currencyIcon.image = UIImage(named:"ExitIconDefault")
+        case .halfSmartSplitPressed:
+            currencyIcon.image = UIImage(named:"SplitIconDefault")
+        case .halfEqualSplitPressed:
+            currencyIcon.image = UIImage(named:"EqualIconDefault")
+        case .halfSharePressed:
+            currencyIcon.image = UIImage(named:"ShareIconDefault")
+        case .halfExitPressed:
+            currencyIcon.image = UIImage(named:"ExitIconDefault")
+        case .halfSmartSplitDeactivate:
+            currencyIcon.image = UIImage(named:"SplitIconDeactivate")
+        case .halfEqualSplitDeactivate:
+            currencyIcon.image = UIImage(named:"EqualIconDeactivate")
+        case .halfShareDeactivate:
+            currencyIcon.image = UIImage(named:"ShareIconDeactivate")
+        case .halfExitDeactivate:
+            currencyIcon.image = UIImage(named:"ExitIconDeactivate")
+        }
+    }
+    
+    private func setCurrencyLabel(style: SPButton.Style) {
+        self.addSubview(currencyLabel)
+        
+        currencyLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(25)
+        }
+        
+        switch style {
+        case .primaryCalmshell:
+            break
+        case .primaryWatermelon:
+            break
+        case .primaryCherry:
+            break
+        case .primaryPear:
+            break
+        case .primaryMushroom:
+            break
+        case .primaryRadish:
+            break
+        case .secondary:
+            break
+        case .warningRed:
+            break
+        case .primaryCalmshellPressed:
+            break
+        case .primaryWatermelonPressed:
+            break
+        case .primaryCherryPressed:
+            break
+        case .primaryPearPressed:
+            break
+        case .primaryMushroomPressed:
+            break
+        case .primaryRadishPressed:
+            break
+        case .secondaryPressed:
+            break
+        case .warningRedPressed:
+            break
+        case .squarePrimaryCalmshell:
+            break
+        case .squareWarningRed:
+            break
+        case .deactivate:
+            break
+        case .squareDeactivate:
+            break
+        case .halfSmartSplit:
+            self.configureHalfButtonStringSmartSplitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfEqualSplit:
+            self.configureHalfButtonStringEqualSplitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfShare:
+            self.configureHalfButtonStringShareProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfExit:
+            self.configureHalfButtonStringExitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfSmartSplitPressed:
+            self.configureHalfButtonStringSmartSplitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfEqualSplitPressed:
+            self.configureHalfButtonStringEqualSplitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfSharePressed:
+            self.configureHalfButtonStringShareProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfExitPressed:
+            self.configureHalfButtonStringExitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfSmartSplitDeactivate:
+            self.configureHalfButtonStringSmartSplitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfEqualSplitDeactivate:
+            self.configureHalfButtonStringEqualSplitProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfShareDeactivate:
+            self.configureHalfButtonStringShareProperties()
+            self.configureHalfButtonFontProperties()
+            
+        case .halfExitDeactivate:
+            self.configureHalfButtonStringExitProperties()
+            self.configureHalfButtonFontProperties()
+        }
+    }
+    
+    // 하프 버튼 폰트 및 타입페이스 설정
+    private func configureHalfButtonFontProperties() {
+//        currencyLabel.font = UIFont.KoreanButtonText
+    }
+    
+    private func configureHalfButtonStringSmartSplitProperties() {
+        currencyLabel.text = "쓴 만큼 정산하기"
+    }
+    
+    private func configureHalfButtonStringEqualSplitProperties() {
+        currencyLabel.text = "1/n 정산하기"
+    }
+    
+    private func configureHalfButtonStringShareProperties() {
+        currencyLabel.text = "친구와 공유하기"
+    }
+    
+    private func configureHalfButtonStringExitProperties() {
+        currencyLabel.text = "스플리릿 끝내기"
     }
 }
