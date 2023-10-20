d028c18a799c97fa14991de6de43420b3e3577da
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2023-10-20 16:44:14 +0900

    [Feat/#23] HistoryView 디테일 작업 제외 완료

---
+++ SplitIt/Presenter/History/CreateDateSection.swift
@@ -0,0 +1,31 @@
+//
+//  CreateDateSection.swift
+//  SplitIt
+//
+//  Created by Zerom on 2023/10/19.
+//
+
+import Foundation
+import RxDataSources
+
+struct CreateDateSection {
+    var header: String
+    var items: [Item]
+    
+    init(items: [Split]) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy. MM. dd"
+        
+        self.header = dateFormatter.string(from: items.first?.createDate ?? Date.now)
+        self.items = items
+    }
+}
+
+extension CreateDateSection: SectionModelType {
+    typealias Item = Split
+    
+    init(original: Self, items: [Item]) {
+        self = original
+        self.items = items
+    }
+}
