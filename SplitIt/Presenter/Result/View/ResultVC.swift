ebbfac597f354dd6a76f660fa5debcf65e0cba89
hsw1920 <hsw1920@gmail.com>
2023-10-23 16:53:58 +0900

    [Feat/#38] ResultView의 레이아웃 및 구현된 비즈니스 로직 적용 - Split관련 title,date를 바인딩 - CompositionalLayout을 통해 unique한 exclMember를 RxDataSource를 통해 구현하여 동적인 DynamicCollectionViewHeight 구현 - CompositionalLayout으로 unique한 exclMember와 연관된 exclItem의 리스트를 구현하여 동적인 DynamicCollectionViewHeight 구현 - collectionView의 header의 toggleButton을 통하여 각 섹션을 fold-unfold 할 수 있도록 구현 - 선택한 section은 unfold되고 나머지 section은 fold되는 toggleButton의 로직 구현 - 선택한 section은 unfold되면서 해당 section의 height가 늘어나고 숨겨진 CSInfo를 display하는 로직 구현

--- SplitIt/Presenter/Result/View/ResultVC.swift
+++ SplitIt/Presenter/Result/View/ResultVC.swift
@@ -85,13 +85,14 @@ class ResultVC: UIViewController {
                         ofKind: UICollectionView.elementKindSectionHeader)
         }
         
+        //TODO: 토글버튼 이벤트 VM에 주고 collectionview를 reload하기
         dataSource = RxCollectionViewSectionedReloadDataSource<ResultSection>(
-            configureCell: { [weak self] dataSource, tableView, indexPath, item in
-                guard let self = self else { return UICollectionViewCell() }
+            configureCell: { dataSource, collectionView, indexPath, item in
                 let section = dataSource.sectionModels[indexPath.section]
                 let cell = collectionView.dequeueReusableCell(for: indexPath, cellType: ResultCell.self)
                 let item = section.items[indexPath.row]
                 cell.configure(item: item)
+                
                 return cell
             }, configureSupplementaryView: { dataSource, collectionView, kind, indexPath in
                 
@@ -110,12 +111,20 @@ class ResultVC: UIViewController {
                     } else {
                         header.configureUnfold(item: section.header, sectionIndex: indexPath.section)
                     }
+                    
                     header.toggleButton.rx.tap
                         .asDriver()
                         .drive(onNext: { [weak self] _ in
                             guard let self = self else { return }
                             // TODO: 나중엔 section 자체의 isFold도 바꿔줘서 collectionView를 다시그려주도록 해야함
-                            self.collectionView.collectionViewLayout = DisplayLayoutFactory.createResultLayout(tappedIndex: indexPath.section, lastSection: addSplitSection)
+                            
+                            let layout = DisplayLayoutFactory.createResultLayout(tappedIndex: indexPath.section, lastSection: addSplitSection)
+                            layout.register(ResultSectionBackground.self, forDecorationViewOfKind: ResultSectionBackground.reuseIdentifier)
+                            collectionView.collectionViewLayout = layout
+
+                            UIView.performWithoutAnimation {
+                                collectionView.reloadSections([indexPath.section, addSplitSection], animationStyle: .none)
+                            }
                             
                             var sections = viewModel.sections.value
                             sections.enumerated().forEach { index, section in
@@ -133,7 +142,6 @@ class ResultVC: UIViewController {
                 }
             }
         )
-        
     }
     
     func setLayout() {
@@ -179,6 +187,14 @@ class ResultVC: UIViewController {
             .drive(collectionView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
         
+        output.splitTitle
+            .drive(splitTitle.rx.text)
+            .disposed(by: disposeBag)
+        
+        output.splitDateString
+            .drive(splitDate.rx.text)
+            .disposed(by: disposeBag)
+
         output.presentResultView
             .drive(onNext: { [weak self] _ in
                 guard let self = self else { return }
@@ -188,217 +204,5 @@ class ResultVC: UIViewController {
                 //                self.navigationController?.pushViewController(vc, animated: true)
             })
             .disposed(by: disposeBag)
-        
     }
-    
 }
-
-//// TODO: ResultView에서 쓰일 foldLayout
-///// 마지막 차수가 열려있고 나머지는 닫혀있어야함.
-//extension ResultVC {
-//    func createLayout() -> UICollectionViewLayout {
-//        let estimatedHeight: CGFloat = 28
-//        let estimatedWidth: CGFloat = 80
-//
-//        // MARK: Section -> ExclItem이 item으로 존재하는 Section
-//        let itemSize = NSCollectionLayoutSize(
-//            widthDimension: .estimated(estimatedWidth),
-//            heightDimension: .absolute(estimatedHeight)
-//        )
-//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-//        item.edgeSpacing = NSCollectionLayoutEdgeSpacing(
-//            leading: .fixed(8),
-//            top: .fixed(0),
-//            trailing: .fixed(0),
-//            bottom: .fixed(0)
-//        )
-//
-//        let groupSize = NSCollectionLayoutSize(
-//            widthDimension: .fractionalWidth(1.0),
-//            heightDimension: .estimated(estimatedHeight)
-//        )
-//        let group = NSCollectionLayoutGroup.horizontal(
-//            layoutSize: groupSize,
-//            subitems: [item]
-//        )
-//        group.contentInsets = NSDirectionalEdgeInsets(
-//            top: 0,
-//            leading: 8,
-//            bottom: 0,
-//            trailing: 8
-//        )
-//
-//        let section = NSCollectionLayoutSection(group: group)
-//        section.interGroupSpacing = 8
-//        section.contentInsets = NSDirectionalEdgeInsets(top: 16,
-//                                                        leading: 0,
-//                                                        bottom: 16,
-//                                                        trailing: 0)
-//
-//        // MARK: ExclItem 관련 Background, decoration View
-//        let header = NSCollectionLayoutBoundarySupplementaryItem(
-//            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-//                                               heightDimension: .absolute(25)),
-//            elementKind: UICollectionView.elementKindSectionHeader,
-//            alignment: .topLeading,
-//            absoluteOffset: CGPoint(x: 0, y: 0))
-//        section.boundarySupplementaryItems = [header]
-//
-//        let sectionBackground = NSCollectionLayoutDecorationItem.background(elementKind: ResultSectionBackground.reuseIdentifier)
-//        section.decorationItems = [ sectionBackground ]
-//
-//        // MARK: Section -> 따로 계산할 항목 더 추가하기 버튼이 존재하는 Section
-//        let addButtonHeight = 104.0
-//        let addSectionItemSize = NSCollectionLayoutSize(
-//            widthDimension: .fractionalWidth(1),
-//            heightDimension: .absolute(addButtonHeight)
-//        )
-//        let addSectionItem = NSCollectionLayoutItem(layoutSize: addSectionItemSize)
-//        let addSectionGroup = NSCollectionLayoutGroup.horizontal(layoutSize: addSectionItemSize,
-//                                                                 subitems: [addSectionItem])
-//        let addSection = NSCollectionLayoutSection(group: addSectionGroup)
-//
-//        // MARK: Layout 정의
-//        let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
-//            return sectionIndex == self.viewModel.sections.value.count - 1 ? addSection : section
-//        }
-//
-//        // MARK: Layout Config 정의
-//        let config = UICollectionViewCompositionalLayoutConfiguration()
-//        config.interSectionSpacing = 16
-//        layout.configuration = config
-//
-//        return layout
-//    }
-//
-//
-//    func foldLayout(index: Int) -> UICollectionViewLayout {
-//        let estimatedHeight: CGFloat = 28
-//        let estimatedWidth: CGFloat = 80
-//
-//        // MARK: Section -> ExclItem이 item으로 존재하는 Section
-//        let itemSize = NSCollectionLayoutSize(
-//            widthDimension: .estimated(estimatedWidth),
-//            heightDimension: .absolute(estimatedHeight)
-//        )
-//        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-//        item.edgeSpacing = NSCollectionLayoutEdgeSpacing(
-//            leading: .fixed(8),
-//            top: .fixed(0),
-//            trailing: .fixed(0),
-//            bottom: .fixed(0)
-//        )
-//
-//        let groupSize = NSCollectionLayoutSize(
-//            widthDimension: .fractionalWidth(1.0),
-//            heightDimension: .estimated(estimatedHeight)
-//        )
-//        let group = NSCollectionLayoutGroup.horizontal(
-//            layoutSize: groupSize,
-//            subitems: [item]
-//        )
-//        group.contentInsets = NSDirectionalEdgeInsets(
-//            top: 0,
-//            leading: 8,
-//            bottom: 0,
-//            trailing: 8
-//        )
-//
-//        let section = NSCollectionLayoutSection(group: group)
-//        section.interGroupSpacing = 8
-//        section.contentInsets = NSDirectionalEdgeInsets(top: 16,
-//                                                        leading: 0,
-//                                                        bottom: 16,
-//                                                        trailing: 0)
-//
-//
-//
-//        // MARK: Section -> ExclItem이 item으로 존재하는 Section
-//        let foldItemSize = NSCollectionLayoutSize(
-//            widthDimension: .absolute(0),
-//            heightDimension: .absolute(0)
-//        )
-//        let foldItem = NSCollectionLayoutItem(layoutSize: foldItemSize)
-//
-//
-//        let foldGroupSize = NSCollectionLayoutSize(
-//            widthDimension: .absolute(0),
-//            heightDimension: .absolute(0)
-//        )
-//        let foldGroup = NSCollectionLayoutGroup.horizontal(
-//            layoutSize: foldGroupSize,
-//            subitems: [foldItem]
-//        )
-//        foldGroup.contentInsets = NSDirectionalEdgeInsets(
-//            top: 0,
-//            leading: 0,
-//            bottom: 0,
-//            trailing: 8
-//        )
-//        let foldSection = NSCollectionLayoutSection(group: foldGroup)
-//        foldSection.interGroupSpacing = 0
-//        foldSection.contentInsets = NSDirectionalEdgeInsets(top: 0,
-//                                                        leading: 0,
-//                                                        bottom: 0,
-//                                                        trailing: 0)
-//
-//        // MARK: ExclItem 관련 Background, decoration View
-//        let header = NSCollectionLayoutBoundarySupplementaryItem(
-//            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-//                                               heightDimension: .absolute(27)),
-//            elementKind: UICollectionView.elementKindSectionHeader,
-//            alignment: .topLeading,
-//            absoluteOffset: CGPoint(x: 0, y: 0))
-//
-//        let foldHeader = NSCollectionLayoutBoundarySupplementaryItem(
-//            layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-//                                               heightDimension: .absolute(50)),
-//            elementKind: UICollectionView.elementKindSectionHeader,
-//            alignment: .topLeading,
-//            absoluteOffset: CGPoint(x: 0, y: 0))
-//
-//        foldSection.boundarySupplementaryItems = [foldHeader]
-//
-//
-//
-//        section.boundarySupplementaryItems = [header]
-//
-//        let sectionBackground = NSCollectionLayoutDecorationItem.background(elementKind: ResultSectionBackground.reuseIdentifier)
-//        section.decorationItems = [ sectionBackground ]
-//
-//        // MARK: Section -> 따로 계산할 항목 더 추가하기 버튼이 존재하는 Section
-//        let addButtonHeight = 104.0
-//        let addSectionItemSize = NSCollectionLayoutSize(
-//            widthDimension: .fractionalWidth(1),
-//            heightDimension: .absolute(addButtonHeight)
-//        )
-//        let addSectionItem = NSCollectionLayoutItem(layoutSize: addSectionItemSize)
-//        let addSectionGroup = NSCollectionLayoutGroup.horizontal(layoutSize: addSectionItemSize,
-//                                                                 subitems: [addSectionItem])
-//        let addSection = NSCollectionLayoutSection(group: addSectionGroup)
-//
-//        // MARK: Layout 정의
-//        let layout = UICollectionViewCompositionalLayout { (sectionIndex, environment) -> NSCollectionLayoutSection? in
-//
-//            if sectionIndex == self.viewModel.sections.value.count - 1 {
-//                return addSection
-//            } else if sectionIndex == index {
-//                return foldSection
-//            } else {
-//                return section
-//            }
-//
-//            //return sectionIndex == self.viewModel.sections.value.count - 1 ? addSection : section
-//        }
-//
-//        // MARK: Layout Config 정의
-//        let config = UICollectionViewCompositionalLayoutConfiguration()
-//        config.interSectionSpacing = 16
-//        layout.configuration = config
-//
-//        return layout
-//    }
-//}
-//
-//
-//
