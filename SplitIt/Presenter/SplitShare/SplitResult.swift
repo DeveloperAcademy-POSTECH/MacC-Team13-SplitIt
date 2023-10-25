9f1062583a1072a4128368f667ca3a46d04e174f
hsw1920 <hsw1920@gmail.com>
2023-10-25 20:08:16 +0900

    [Feat/#106] SplitShare 공유뷰에서 필요한 모델 작성

---
+++ SplitIt/Presenter/SplitShare/SplitResult.swift
@@ -0,0 +1,14 @@
+//
+//  SplitResult.swift
+//  SplitIt
+//
+//  Created by Zerom on 2023/10/23.
+//
+
+import Foundation
+
+struct SplitMemberResult {
+    var memberName: String
+    var memberPrice: Int
+    var exclDatas: [String]
+}
