46e86ceb88368824356ec3a9f2c39c50d7c99630
Zerom (Junho Jin) <111134273+jincode93@users.noreply.github.com>
2023-10-26 21:10:08 +0900

    [Fix/#111] Share 기능 예외 처리 및 로직 VM으로 이동, Share 기능 구현 완료

--- SplitIt/Resources/Extensions/UITableView+.swift
+++ SplitIt/Resources/Extensions/UITableView+.swift
@@ -9,33 +9,26 @@ import UIKit
 
 extension UITableView {
     func screenshot() -> UIImage? {
+        // self.scrollToRow(at: lastIndex, at: UITableView.ScrollPosition.none, animated: false)
+        
         let contentWidthOrigin = self.contentSize.width
         let contentHeightOrigin = self.contentSize.height
-        
+
         let savedContentOffset = self.contentOffset
         let savedFrame = self.frame
-        
-//        if contentHeightOrigin >= UIScreen.main.bounds.height {
-//
-//
-//            let contentWidthTrans = contentWidthOrigin * UIScreen.main.bounds.height / contentHeightOrigin
-//            let contentHeightTrans = UIScreen.main.bounds.height
-//        } else {
-//            self.layer.frame = CGRect(x: 0, y: 0, width: contentWidthOrigin, height: contentHeightOrigin)
-//        }
-//
+
         self.contentOffset = CGPoint.zero
-        
         self.layer.frame = CGRect(x: 0, y: 0, width: contentWidthOrigin, height: contentHeightOrigin)
-        
-        UIGraphicsBeginImageContext(CGSize(width: contentWidthOrigin, height: contentHeightOrigin))
-        
+
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: contentWidthOrigin, height: contentHeightOrigin), false, 2.0)
+
         self.layer.render(in: UIGraphicsGetCurrentContext()!)
         let image = UIGraphicsGetImageFromCurrentImageContext()
-        
+
         self.contentOffset = savedContentOffset
         self.frame = savedFrame
         UIGraphicsEndImageContext()
+        
         return image
     }
 }
