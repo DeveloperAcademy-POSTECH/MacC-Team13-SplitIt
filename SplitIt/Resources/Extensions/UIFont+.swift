4383c14250288434091c808372efd04b592a7790
pscu91 <91787174+pscu91@users.noreply.github.com>
2023-11-17 15:21:31 +0900

    [Feat/#286] 영수증 서체 추가

--- SplitIt/Resources/Extensions/UIFont+.swift
+++ SplitIt/Resources/Extensions/UIFont+.swift
@@ -7,8 +7,112 @@
 
 import UIKit
 
-extension UIFont {
+struct AppFontName {
+    static let OneL = "ONEMobileLight"
+    static let OneR = "ONEMobileRegular"
+    static let OneB = "ONEMobileBold"
+    static let EdcR = "EliceDigitalCodingver.H"
+    static let EdcB = "EliceDigitalCodingver.H-Bd"
+}
 
+extension UIFont {
+    
+    // MARK: Receipt Typeface
+    
+    /**
+     30pt / Receipt / Title 1
+     - parameters:
+        - property: $receipt-title1
+     */
+    class var ReceiptTitle1: UIFont {
+        switch UIDevice().screenType {
+        case .iPhoneSE:
+            return UIFont(name: AppFontName.EdcR, size: 28)!
+        case .defaultDevice:
+            return UIFont(name: AppFontName.EdcR, size: 30)!
+        }
+    }
+
+    /**
+     27pt / Receipt / Title 2
+     - parameters:
+        - property: $receipt-title2
+     */
+    class var ReceiptTitle2: UIFont {
+        switch UIDevice().screenType {
+        case .iPhoneSE:
+            return UIFont(name: AppFontName.EdcR, size: 25)!
+        case .defaultDevice:
+            return UIFont(name: AppFontName.EdcR, size: 27)!
+        }
+    }
+
+    /**
+     24pt / Receipt / Title 3
+     - parameters:
+        - property: $receipt-title3
+     */
+    class var ReceiptTitle3: UIFont {
+        switch UIDevice().screenType {
+        case .iPhoneSE:
+            return UIFont(name: AppFontName.EdcR, size: 22)!
+        case .defaultDevice:
+            return UIFont(name: AppFontName.EdcR, size: 24)!
+        }
+    }
+
+    /**
+     21pt / Receipt / Subtitle
+     - parameters:
+        - property: $receipt-subtitle
+     */
+    class var ReceiptSubtitle: UIFont {
+        switch UIDevice().screenType {
+        case .iPhoneSE:
+            return UIFont(name: AppFontName.EdcR, size: 19)!
+        case .defaultDevice:
+            return UIFont(name: AppFontName.EdcR, size: 21)!
+        }
+    }
+
+    /**
+     18pt / Receipt / Body
+     - parameters:
+        - property: $receipt-body
+     */
+    class var ReceiptBody: UIFont {
+        switch UIDevice().screenType {
+        case .iPhoneSE:
+            return UIFont(name: AppFontName.EdcR, size: 16)!
+        case .defaultDevice:
+            return UIFont(name: AppFontName.EdcR, size: 18)!
+        }
+    }
+
+    /**
+     15pt / Receipt / Caption 1
+     - parameters:
+        - property: $receipt-caption1
+     */
+    class var ReceiptCaption1: UIFont {
+        switch UIDevice().screenType {
+        case .iPhoneSE:
+            return UIFont(name: AppFontName.EdcR, size: 14)!
+        case .defaultDevice:
+            return UIFont(name: AppFontName.EdcR, size: 15)!
+        }
+    }
+
+    /**
+     12pt / Receipt / Caption 2
+     - parameters:
+        - property: $receipt-caption2
+     */
+    class var ReceiptCaption2: UIFont {
+        return UIFont(name: AppFontName.EdcR, size: 12)!
+    }
+    
+    
     // MARK: Korean Typeface
 
     /**
@@ -20,9 +124,9 @@ extension UIFont {
     class var KoreanTitle1: UIFont {
         switch UIDevice().screenType {
         case .iPhoneSE:
-            return UIFont(name: "ONEMobileRegular", size: 28)!
+            return UIFont(name: AppFontName.OneR, size: 28)!
         case .defaultDevice:
-            return UIFont(name: "ONEMobileRegular", size: 30)!
+            return UIFont(name: AppFontName.OneR, size: 30)!
         }
     }
 
@@ -34,9 +138,9 @@ extension UIFont {
     class var KoreanTitle2: UIFont {
         switch UIDevice().screenType {
         case .iPhoneSE:
-            return UIFont(name: "ONEMobileRegular", size: 25)!
+            return UIFont(name: AppFontName.OneR, size: 25)!
         case .defaultDevice:
-            return UIFont(name: "ONEMobileRegular", size: 27)!
+            return UIFont(name: AppFontName.OneR, size: 27)!
         }
     }
 
@@ -48,9 +152,9 @@ extension UIFont {
     class var KoreanTitle3: UIFont {
         switch UIDevice().screenType {
         case .iPhoneSE:
-            return UIFont(name: "ONEMobileRegular", size: 22)!
+            return UIFont(name: AppFontName.OneR, size: 22)!
         case .defaultDevice:
-            return UIFont(name: "ONEMobileRegular", size: 24)!
+            return UIFont(name: AppFontName.OneR, size: 24)!
         }
     }
 
@@ -62,9 +166,9 @@ extension UIFont {
     class var KoreanSubtitle: UIFont {
         switch UIDevice().screenType {
         case .iPhoneSE:
-            return UIFont(name: "ONEMobileRegular", size: 19)!
+            return UIFont(name: AppFontName.OneR, size: 19)!
         case .defaultDevice:
-            return UIFont(name: "ONEMobileRegular", size: 21)!
+            return UIFont(name: AppFontName.OneR, size: 21)!
         }
     }
 
@@ -76,9 +180,9 @@ extension UIFont {
     class var KoreanBody: UIFont {
         switch UIDevice().screenType {
         case .iPhoneSE:
-            return UIFont(name: "ONEMobileRegular", size: 16)!
+            return UIFont(name: AppFontName.OneR, size: 16)!
         case .defaultDevice:
-            return UIFont(name: "ONEMobileRegular", size: 18)!
+            return UIFont(name: AppFontName.OneR, size: 18)!
         }
     }
 
@@ -88,7 +192,7 @@ extension UIFont {
         - property: $korean-button-text
      */
     class var KoreanButtonText: UIFont {
-        return UIFont(name: "ONEMobileBold", size: 16)!
+        return UIFont(name: AppFontName.OneB, size: 16)!
     }
 
     /**
@@ -97,7 +201,7 @@ extension UIFont {
         - property: $korean-small-button-text
      */
     class var KoreanSmallButtonText: UIFont {
-        return UIFont(name: "ONEMobileBold", size: 12)!
+        return UIFont(name: AppFontName.OneB, size: 12)!
     }
 
     /**
@@ -108,9 +212,9 @@ extension UIFont {
     class var KoreanCaption1: UIFont {
         switch UIDevice().screenType {
         case .iPhoneSE:
-            return UIFont(name: "ONEMobileRegular", size: 14)!
+            return UIFont(name: AppFontName.OneR, size: 14)!
         case .defaultDevice:
-            return UIFont(name: "ONEMobileRegular", size: 15)!
+            return UIFont(name: AppFontName.OneR, size: 15)!
         }
     }
 
@@ -120,6 +224,6 @@ extension UIFont {
         - property: $korean-caption2
      */
     class var KoreanCaption2: UIFont {
-        return UIFont(name: "ONEMobileLight", size: 12)!
+        return UIFont(name: AppFontName.OneL, size: 12)!
     }
 }
